293f46592e2a485043fcae5e84c6473ca5f46837
iamanish1 <animetra14@gmail.com>
2025-02-03 22:43:23 +0530

    done with talwind css installation also

--- client/src/Pages/BugHuntArenaLeaderBoard.jsx
+++ client/src/Pages/BugHuntArenaLeaderBoard.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
 const BugHuntArenaLeaderBoard = ()=>{
     return (
         <>
         {/* Anish need ot codehere  */}
+      
         </>
     )
 }; 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable no-unused-vars */
 import { StrictMode } from 'react'
+import React from "react";
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import BugHuntArenaParticipantPage from './Pages/BugHuntArenaParticipant.jsx'
+import BugHuntArenaLeaderBoard from './Pages/BugHuntArenaLeaderBoard.jsx'
+import EventRegistrationPage from './Pages/EventRegisterationPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BugHuntArenaParticipantPage/>
+    <BugHuntArenaLeaderBoard/>
+    <EventRegistrationPage/>
+  </StrictMode>
 )
